57587adf53622e84c9536bb1a4bccad204469197
Neon Ratemo <neon.ratemo@student.moringaschool.com>
2023-08-20 12:27:33 +0300

    wueh ni God

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy new year")
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return int_list * int_list
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("fizzbuzz")
+        elif i % 3 == 0:
+            print("fizz")
+        elif i % 5 == 0:
+            print("Buzz")
